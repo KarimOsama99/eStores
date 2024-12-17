8fd251f44916e0487c21053c539700d630c31d3c
karimosama99 <ko5418301@gmail.com>
2024-12-17 02:19:17 +0200

    dashboard

---
+++ register/js/select.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+ $('.js-example-basic-single').select2({
+    placeholder: "Buisenes Activity"
+ });
+ $(".js-example-placeholder-multiple").select2({
+   placeholder: "Choose a sub activity",
+ });
+
+});
